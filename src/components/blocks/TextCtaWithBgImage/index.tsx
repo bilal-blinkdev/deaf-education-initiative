29f2f380951a39a795ed824a7e5069b6f1196266
Bilal <bilal@blinkco.io>
2025-11-05 18:09:33 +0500

    chore: update text on about page

--- src/components/blocks/TextCtaWithBgImage/index.tsx
+++ src/components/blocks/TextCtaWithBgImage/index.tsx
@@ -23,11 +23,10 @@ export default function TextCtaWithBgImage({ sectionClass, customClass }: TextCt
         <div className={styles.flexBox}>
           <h2 className={styles.heading}>Organization Information</h2>
           <p className={styles.description}>
-            Deaf Education Initiative (Charity Commission No. 1209822) is a UK-registered charity
-            that supports the education of underprivileged deaf children in Pakistan. The Initiative
-            provides funding and strategic support for the Deaf Reach program run by the Family
-            Educational Services Foundation (FESF) in Pakistan, ensuring access to schooling,
-            transportation, and essential resources for deaf children and their families.
+            Deaf Education Initiative is a UK-registered charity (charity number 1209822) that is
+            committed to empowering underprivileged deaf children and youth in Pakistan. Through the
+            Deaf Reach program in Pakistan, we provide education, skills training, and job placement
+            programs to empower deaf children.
           </p>
           <Button size="large" icons={{ leading: true }}>
             View Certification
